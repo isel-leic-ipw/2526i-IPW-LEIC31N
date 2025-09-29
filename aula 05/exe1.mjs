ef6f45678b770b7dad9da8d74b36764ba91f0d91
Fstn75 <jose.faustino@gmail.com>
2025-09-29 19:27:21 +0100

    change

---
+++ aula 05/exe1.mjs
@@ -0,0 +1,19 @@
+
+function validateArrayElements (arr, elementValidator) {
+    // funcao de exemplo
+    
+    
+    
+}
+
+const numbers = [2, 3, 4, 5];
+console.log(validateArrayElements(numbers, n => n % 2 === 0));
+/*
+[
+ { value: 2, isValid: true },
+ { value: 3, isValid: false },
+ { value: 4, isValid: true },
+ { value: 5, isValid: false }
+]
+*/
+
