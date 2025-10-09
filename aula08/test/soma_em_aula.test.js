511fa1aac1d9eac055d019bc33a6da080b353602
Fstn75 <jose.faustino@gmail.com>
2025-10-09 20:24:01 +0100

    soma

--- aula08/test/soma_em_aula.test.js
+++ aula08/test/soma_em_aula.test.js
@@ -4,16 +4,16 @@ npm install assert
 */
 
 import assert from 'assert';
-import {somatorio} from '../../aula02/modulo.js'
+import * as aritmetica from '../../aula02/modulo.js'
 
 describe ('Teste01 - Testar a soma', function () {
     it ('Step 1', function (){
-        let total = somatorio (10,5);
+        let total = aritmetica.somatorio (10,5);
        assert.equal(total, 15);
     }),
         it ('Step 2', function (){
-        let total = somatorio (10,5);
-        assert.equal(total, 10);
+        let total = aritmetica.somatorio (10,5);
+        assert.notEqual(total, 10);
     })
 }
 );
